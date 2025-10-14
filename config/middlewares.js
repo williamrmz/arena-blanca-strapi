74892dfaf83a949c5d38cbc5d03ab97fab2ac649
William <williamxtez14@gmail.com>
2025-10-14 07:47:46 -0500

    add origin *

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,13 +5,7 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: [
-        'http://localhost:3000',
-        'http://localhost:4321',
-        'https://reservarenablanca.com',
-        'https://www.reservarenablanca.com',
-        'https://arena-blanca-strapi-production.up.railway.app'
-      ],
+      origin: '*',
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
